9d984cccaacfc1ff10644a525ec69e0e517c7352
Diego Neri <diego@diegoneri.com>
2021-03-15 07:48:37 -0300

    Corrige endentação

--- Program.cs
+++ Program.cs
@@ -1,36 +1,27 @@
-﻿using System;
+using System;
 
-namespace Gritador
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Gritador {
+    class Program {
+        static void Main(string[] args) {
             Console.Clear();
             Console.Beep();
             Console.BackgroundColor = ConsoleColor.Yellow;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("VAMOS BRINCAR EM DEIXAR TUDO MAISCULO, AQUILO QUE O AMIGUINHO DIGITAR");
             Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.White;
-;           Console.WriteLine("Olá prazer me chamo Fabiano e você, como se chama? ");
+            Console.ForegroundColor = ConsoleColor.White;;
+            Console.WriteLine("Olá prazer me chamo Fabiano e você, como se chama? ");
             string nome = Console.ReadLine();
 
-            if (nome == nome.ToUpper())
-        {
-            Console.WriteLine("Desligue o CAPS LOK");
-     }
-           else
-       {
-    
-
-            Console.WriteLine ($"Seja bem vindo, {nome.ToUpper()}, como foi o seu dia? ");
-            string textoDigitado = Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"{textoDigitado.ToUpper()}, Ual!!! {nome.ToUpper()}, o que houve? no proximo encontro quero saber, Tchau.");
-            Console.ResetColor();
-          }
+            if (nome == nome.ToUpper()) {
+                Console.WriteLine("Desligue o CAPS LOK");
+            } else {
+                Console.WriteLine($"Seja bem vindo, {nome.ToUpper()}, como foi o seu dia? ");
+                string textoDigitado = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"{textoDigitado.ToUpper()}, Ual!!! {nome.ToUpper()}, o que houve? no proximo encontro quero saber, Tchau.");
+                Console.ResetColor();
+            }
         }
     }
 }
-
